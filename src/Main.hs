5ccff1835e0e874693d029ffe88931359a430ef9
jeffrey thornton <jeffreyjthornton@gmail.com>
2012-04-07 23:32:08 -0500

    added method for caching intermediate results to speed up testing

---
+++ src/Main.hs
@@ -0,0 +1,49 @@
+module Main {-(main)-} where
+
+import Input
+import FFT
+import Convert
+
+import Control.Monad
+
+import Graphics.Gnuplot.Simple
+
+--------------------------------------------
+
+{-	useful for caching fft result
+-}
+load :: FilePath -> IO [Double]
+load f = do
+	s <- readFile f
+	return (read s :: [Double])
+
+
+save :: (Show a) => a -> FilePath -> IO ()
+save x f = writeFile f (show x)
+
+---------------------------------------------
+
+wavPath = "../test/t2.wav"
+freqPath = "../test/t2.freq"
+ampPath = "../test/t2.amp"
+
+rawfft = liftM fftout (waveIn wavPath)
+
+freq = liftM toFreq rawfft
+amp = liftM toAmp rawfft
+peak = liftM toPeak rawfft
+
+main = do
+	a <- rawfft
+	save (toFreq a) freqPath
+	save (toAmp a) ampPath
+	print "ok!" 
+
+
+{-
+
+freq <- load freqPath; amp <- load ampPath
+
+plotList [] freq
+
+-}
